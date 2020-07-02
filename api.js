e974bb6cee86b88dd8254102b3c708606502ffdc
TWCurry <thomas.w.curry@accenture.com>
2020-07-02 11:17:28 +0100

    Added outline of api code

---
+++ api.js
@@ -0,0 +1,25 @@
+// Imports
+var express = require('express');
+
+// Globals
+var port = 3000;
+
+// Initialise Express
+var app = express();
+app.listen(port, ()=> console.log(`API running at http://localhost:${port}`));
+
+// Api Methods
+
+// Add record(s)
+
+// Update record(s)
+
+// Show total number of cases
+
+// Delete record(s)
+
+// Fetch first 20 records
+
+// Display states where cases > 1 in a single day
+
+// Display device information
