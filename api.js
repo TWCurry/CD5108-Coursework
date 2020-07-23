1ebc282f1b5631bfbd52a8c51b53171972d4a716
TWCurry <thomas.w.curry@accenture.com>
2020-07-23 20:53:55 +0100

    Removed unused variable

--- api.js
+++ api.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient
 var bodyParser = require("body-parser");
-const { query } = require('express');
 
 // Globals
 var port = 3000;
