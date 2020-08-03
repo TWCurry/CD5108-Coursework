880e6f124b1e853407f31fde6d619699818bff43
TWCurry <thomas.w.curry@accenture.com>
2020-08-03 21:14:34 +0100

    updates

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 // Imports
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var util = require('util');
-var os = require("os");
+var os = require('os');
 
 // Globals
 var port = 3000;
@@ -57,8 +57,8 @@ dbConn.then(function (client){
             try {
                 let query = {"state": req.body.updateState, "county": req.body.updateCounty};
                 let newData = { $set: {"date": req.body.updateDate, "cases": req.body.updateCases, "deaths": req.body.updateDeaths}};
-            collection.updateOne(query, newData);
-            res.send("Data successfully updated.");
+                collection.updateOne(query, newData);
+                res.send("Data successfully updated.");
             } catch (error) {
                 displayErrorPage(res, `Could not write to DB - ${error}`)
             }
@@ -66,41 +66,21 @@ dbConn.then(function (client){
     });
 
     // Show total number of cases
-    app.get('/viewTotalCases', function(req, res){
-        if (!req.query.displayState || !req.query.displayCounty){
-            console.log("Incorrect data sent ")
-            displayErrorPage(res, "Missing data - Incorrect data inserted.");
-        } else {
-            collection.find({county:req.query.displayCounty, state:req.query.displayState},{ _id:0}).sort({date:-1}).limit(1).toArray(function(err,result){
-                if (err) throw err;
-                console.log(result);
-                res.send(result);
-            })
-        }
-    })
 
     // Delete record(s)
 
     // Fetch first 20 records
 
     // Display states where cases > 1 in a single day
+    app.get('/statesWithMultipleCases', function (req, res) {
+        var query = { deaths: /^0*(?:[2-9]|[1-9]\d\d*)$/g }; // Regex string that matches any number higher than one
+        collection.find(query).toArray(function(err, result) {
+            console.log(result);
+        });
+    });
 
     // Display device information
-    app.get('/displayDeviceInfo', function(req,res){
-        res.send(
-            '<html><head><title>Operating System Info</title></head>'+
-            '<body><h1>Operating System Info</h1>'+
-            '<table>'+
-            '<tr><th>Temp Dir</th><td>' + os.tmpdir() + '</td></tr>'+
-            '<tr><th>Host Name</th><td>' + os.hostname() + '</td></tr>'+
-            '<tr><th>Type of OS</th><td>' + os.type() + os.platform()+ os.arch()+ os.release()+ '</td></tr>'+
-            '<tr><th>Uptime</th><td>'+(os.uptime())/3600+'hours.userInfo'+util.inspect(os.userInfo())+'</td></tr>'+
-            '<tr><th>Memory</th><td>total:'+os.totalmem()+'free:'+os.freemem()+'</td></tr>'+
-            '<tr><th>CPU</th><td>'+util.inspect(os.cpus())+'</td></tr>'+
-            '<tr><th>Network</th><td>'+util.inspect(os.networkInterfaces())+'</td></tr>'+
-            '</table>'+'</body></html>'
-            )
-    })
+
     // Display 404 page
     app.get('*', function(req, res){
         res.sendFile('webpage/404.html', { root: __dirname});
