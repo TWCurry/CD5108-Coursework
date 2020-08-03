e5f66cdc180114478c78219ba3976ff3c61a96cf
TWCurry <thomas.w.curry@accenture.com>
2020-08-03 21:37:23 +0100

    manual commit to resolve merge issues

--- api.js
+++ api.js
@@ -67,6 +67,18 @@ dbConn.then(function (client){
     });
 
     // Show total number of cases
+    app.get('/viewTotalCases', function(req, res){
+        if (!req.query.displayState || !req.query.displayCounty){
+            console.log("Incorrect data sent ")
+            displayErrorPage(res, "Missing data - Incorrect data inserted.");
+        } else {
+            collection.find({county:req.query.displayCounty, state:req.query.displayState},{ _id:0}).sort({date:-1}).limit(1).toArray(function(err,result){
+                if (err) throw err;
+                console.log(result);
+                res.send(result);
+            })
+        }
+    })
 
     // Delete record(s)
 
@@ -97,6 +109,21 @@ dbConn.then(function (client){
     });
 
     // Display device information
+    app.get('/displayDeviceInfo', function(req,res){
+        res.send(
+            '<html><head><title>Operating System Info</title></head>'+
+            '<body><h1>Operating System Info</h1>'+
+            '<table>'+
+            '<tr><th>Temp Dir</th><td>' + os.tmpdir() + '</td></tr>'+
+            '<tr><th>Host Name</th><td>' + os.hostname() + '</td></tr>'+
+            '<tr><th>Type of OS</th><td>' + os.type() + os.platform()+ os.arch()+ os.release()+ '</td></tr>'+
+            '<tr><th>Uptime</th><td>'+(os.uptime())/3600+'hours.userInfo'+util.inspect(os.userInfo())+'</td></tr>'+
+            '<tr><th>Memory</th><td>total:'+os.totalmem()+'free:'+os.freemem()+'</td></tr>'+
+            '<tr><th>CPU</th><td>'+util.inspect(os.cpus())+'</td></tr>'+
+            '<tr><th>Network</th><td>'+util.inspect(os.networkInterfaces())+'</td></tr>'+
+            '</table>'+'</body></html>'
+            )
+    })
 
     // Display 404 page
     app.get('*', function(req, res){
