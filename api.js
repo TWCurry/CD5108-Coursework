f77d8719c0dcd582f6e8d2a34af8ad1958ee401a
Mohindru <amit.s.mohindru@accenture.com>
2020-08-07 10:47:28 +0100

    Delete Records

--- api.js
+++ api.js
@@ -79,9 +79,18 @@ dbConn.then(function (client){
             })
         }
     })
-
+    
     // Delete record(s)
-
+    app.delete('/deleteRecords', function (req, res) {
+        if (req.body.insertCounty == "" || req.body.insertState == "" || req.body.insertCases == "" || req.body.insertDeaths == "" || req.body.insertDate == ""){
+            console.log("Incorrect data sent.");
+            displayErrorPage(res, "Missing data. Please ensure all forms are correctly filled.");
+        } else {
+            collection.removeOne({"date": req.body.insertDate, "state": req.body.insertState, "cases": req.body.insertCases, "deaths": req.body.insertDeaths, "county": req.body.insertCounty});
+            console.log("Data deleted.");
+            res.send("Data successfully deleted.");
+        }
+    });
     // Fetch first 20 records
 
     // Display states where cases > 1 in a single day
